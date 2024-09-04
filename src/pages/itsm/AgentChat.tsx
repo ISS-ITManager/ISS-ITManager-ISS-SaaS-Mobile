94b408875a9dd5d93fa781661491bec5606b6716
Marjorie <marjdolotina@gmail.com>
2024-09-04 13:55:03 +0300

    incident, request, agentChat, styles

---
+++ src/pages/itsm/AgentChat.tsx
@@ -0,0 +1,34 @@
+
+
+import MasterComponent from '../../components/layouts/MasterComponent'
+
+const AgentChat = () => {
+  return (
+    <MasterComponent page="itsm">
+      <div className="message-container">
+        <div className="message-bubble sender">
+          <div className="message-info">
+            <p className="message-date">{Date()}</p>
+            <p className="message-subject">Good Day!</p>
+          </div>
+          <div
+            className="message-content"
+          // dangerouslySetInnerHTML={{ __html: }}
+          >How can I help you today?</div>
+        </div>
+      </div>
+
+      <div className="shadow-xs">
+
+        <div className="card-footer text-muted d-flex justify-content-start align-items-center p-3rd">
+            <input type="text" className="form-control"
+              placeholder="Type message"/>
+            <a className="ms-3" href="#!"><i className="fas fa-paper-plane"></i></a>
+        </div>
+    </div>
+
+    </MasterComponent>
+  )
+}
+
+export default AgentChat;
